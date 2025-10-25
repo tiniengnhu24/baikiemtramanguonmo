77d187da55725989c5ebf9d977c4143adb66b00c
tiniengnhu24 <babylovevn159@gmail.com>
2025-10-25 14:00:02 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import "./styles.css";
+// @ts-ignore
+import Home from "./Home";
+// @ts-ignore
+import Layout from "./Layout";
+// @ts-ignore
+import Trang1 from "./Trang1";
+// @ts-ignore
+import Chitietsanpham from "./Chitietsanpham";
+// @ts-ignore
+import ListSanPham from "./ListSanPham";
+// @ts-ignore
+import Trang2 from "./Trang2";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  // return <Layout />;
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="trang1" element={<Trang1 />} />
+          <Route path="sanpham/:id" element={<Chitietsanpham />} />
+          <Route path="trang2" element={<Trang2 />} />
+          <Route path="ListSanPham" element={<ListSanPham />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/Layout.js
@@ -0,0 +1,49 @@
+import "./assets/css/main.css";
+import anhlogo from "./assets/images//Ten-truong-do-1000x159.png";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <html>
+      <header>
+        <div id="divheader" class="header1">
+          <div id="banner" class="banner1">
+            <div id="topleft">
+              <ul class="ul1">
+                <li>
+                  <a href="/#">TRANG CHỦ</a>
+                </li>
+                <li>
+                  <a href="/trang1">EGOV</a>
+                </li>
+                <li>
+                  <a href="/trang2">SINH VIÊN</a>
+                </li>
+                <li>
+                  <a href="/ListSanPham">SẢN PHẨM</a>
+                </li>
+              </ul>
+            </div>
+            <div id="logo" class="logo1">
+              <img src={anhlogo} width="548" />
+            </div>
+            <div id="divtimkiem" style={{ width: "300px" }}>
+              Phần tìm kiếm
+            </div>
+          </div>
+          <div id="divmenu" class="menu">
+            BỎ TRỐNG
+          </div>
+        </div>
+      </header>
+      <body>
+        <div id="container" class="container">
+          <Outlet />
+        </div>
+      </body>
+      <footer></footer>
+    </html>
+  );
+};
+
+export default Layout;
